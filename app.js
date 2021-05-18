528407956dca73240fcce307de7f99783eec9d22
josuevfranco <76227878+josuevfranco@users.noreply.github.com>
2021-05-18 00:41:19 -0500

    Se han actualizado cambios

--- app.js
+++ app.js
@@ -6,21 +6,26 @@
            * pendiente
          )
 */
-
-
-const tareas = require('./tareas')
 const fs = require('fs');
-
+const path = './tareas.json';
 
 exports.leerTareas = function () {
-    const listado = fs.readFileSync(this.path, 'utf-8')
-    return JSON.parse(listado);    
+    /*
+        En esta función debes leer y retornar las tareas registradas.
+    */
+
+    const listado = fs.readFileSync('tareas.json', 'utf-8');
+    const tareas = JSON.parse(listado);
+
+    return tareas;
 }
 
 exports.agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
+    const tareas = JSON.parse(fs.readFileSync('./tareas.json'));
+    fs.writeFileSync(path, JSON.stringify([...tareas, tarea]));
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -28,4 +33,7 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
+
+    const tareas = JSON.parse(fs.readFileSync('./tareas.json'));
+    return tareas.filter(e => e.status === estado);
 }
